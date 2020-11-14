a511d794bc013a26a32a169ec5e562cc6c62bae6
David <daveski084@users.noreply.github.com>
2020-11-13 22:52:27 -0500

    Lab 7b

--- Assets/_Scripts/opossomBehaviour.cs
+++ Assets/_Scripts/opossomBehaviour.cs
@@ -2,6 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+public enum RampDirection
+{
+    NONE,
+    UP,
+    DOWN
+}
+
 public class opossomBehaviour : MonoBehaviour
 {
 
@@ -12,12 +20,15 @@ public class opossomBehaviour : MonoBehaviour
     public LayerMask collisionGroundLayer;
     public LayerMask collisionWallLayer;
     public bool isGroundAhead;
+    public bool onRamp;
+    public RampDirection rampDirection;
     
 
     // Start is called before the first frame update
     void Start()
     {
         rigidbody2D = GetComponent<Rigidbody2D>();
+        rampDirection = RampDirection.NONE;
     }
 
     // Update is called once per frame
@@ -30,9 +41,24 @@ public class opossomBehaviour : MonoBehaviour
 
     private void _LookInFront()
     {
-        if(Physics2D.Linecast(transform.position, lookInFrontPoint.position, collisionWallLayer))
+        var wallHit = Physics2D.Linecast(transform.position, lookInFrontPoint.position, collisionWallLayer);
+
+        if (wallHit)
         {
-            _FlipX();
+            if (!wallHit.collider.CompareTag("Ramps"))
+            {
+
+                if(!onRamp && transform.rotation.z == 0.0f)
+                {
+                    _FlipX();
+                }
+                
+                rampDirection = RampDirection.DOWN;
+            }
+            else
+            {
+                rampDirection = RampDirection.UP;
+            }
         }
 
         Debug.DrawLine(transform.position, lookInFrontPoint.position, Color.red);
@@ -40,7 +66,27 @@ public class opossomBehaviour : MonoBehaviour
 
     private void _LookAhead()
     {
-        isGroundAhead = Physics2D.Linecast(transform.position, lookAheadPoint.position, collisionGroundLayer);
+        var groundHit = Physics2D.Linecast(transform.position, lookAheadPoint.position, 
+            collisionGroundLayer);
+        
+        if (groundHit)
+        {
+            if(groundHit.collider.CompareTag("Ramps"))
+            {
+                onRamp = true;
+            }
+
+            if(groundHit.collider.CompareTag("Platforms"))
+            {
+                onRamp = false;
+            }
+
+            isGroundAhead = true;
+        }
+        else
+        {
+            isGroundAhead = false;
+        }
 
         Debug.DrawLine(transform.position, lookAheadPoint.position, Color.green);
     }
@@ -50,7 +96,27 @@ public class opossomBehaviour : MonoBehaviour
 
         if(isGroundAhead)
         {
-            rigidbody2D.AddForce(Vector2.left * runForce * Time.deltaTime *transform.localScale.x);
+            rigidbody2D.AddForce(Vector2.left * runForce * Time.deltaTime * transform.localScale.x);
+           
+            if(onRamp)
+            {
+                
+                if(rampDirection == RampDirection.UP)
+                {
+                    rigidbody2D.AddForce(Vector2.up * runForce * 0.5f * Time.deltaTime);
+                }
+                else
+                {
+                    rigidbody2D.AddForce(Vector2.down * runForce * 0.25f * Time.deltaTime);
+                }
+
+                StartCoroutine(Rotate());
+            }
+            else
+            {
+                StartCoroutine(Normalize());
+            }
+           
             rigidbody2D.velocity *= 0.90f;
         }
         else
@@ -60,11 +126,22 @@ public class opossomBehaviour : MonoBehaviour
        
     }
 
+    IEnumerator Rotate()
+    {
+        yield return new WaitForSeconds(0.05f);
+        transform.rotation = Quaternion.Euler(0.0f, 0.0f, -26.0f);
+    }
+
+    IEnumerator Normalize()
+    {
+        yield return new WaitForSeconds(0.05f);
+        transform.rotation = Quaternion.Euler(0.0f, 0.0f, 0.0f);
+    }
+
     private void _FlipX()
     {
         transform.localScale = new Vector3(transform.localScale.x * -1.0f, transform.localScale.y,
                 transform.localScale.z);
     }
 
-  
 }
